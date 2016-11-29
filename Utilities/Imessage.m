dd3db74b8ba113cbaa901621a602f9d56779b4c9
Liu <jl54736@austin.eid.utexas.edu>
2016-11-29 14:51:53 -0600

    Organized Utilities. Deleted unncessary mat files

--- Utilities/Imessage.m
+++
@@ -1,7 +0,0 @@
-function []= Imessage()
-disp('****************************************************************')
-disp(' This sript is made for turbulent sea state simulation')
-disp(' Modified by Jinsong Liu on 6/7/2015')
-disp(' NOTE: FOR NOW, only run for one sea state and assume Nsim = 1')
-disp('***************************************************************')
-end
