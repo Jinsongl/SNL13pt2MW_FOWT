d9996658597eccbce09840d6e17aea510b9fbc2f
Liu <jl54736@austin.eid.utexas.edu>
2016-12-14 17:08:21 -0600

    Add function to upload data to Google Drive when simulation is done

--- RunTurbSim_FAST.m
+++ RunTurbSim_FAST.m
@@ -18,9 +18,9 @@ disp('**********  Environment Conditions  **********')
 disp(['Location: ', site])
 disp('Physical random variables:')
 disp(varNames)
-windVel = phyRvs(:,1);
-waveHs  = phyRvs(:,2);
-waveTp  = phyRvs(:,3);
+% windVel = phyRvs(:,1);
+% waveHs  = phyRvs(:,2);
+% waveTp  = phyRvs(:,3);
 %% Define Model Properties
 disp('**********  Model Summaries  **********')
 MODEL_NAME      = 'SNLOffshrBsline13pt2MW';
@@ -34,11 +34,12 @@ disp(['Mooring line model:      ', MOORING_MODEL]);
 
 %% Simulation control
 % Predefined simulation parameters
-Nsim        = 10 ;
-Tmax        = 4000;             % total run time, seconds onshr: 10 mins, offshr: one hour
-waveDirs    = [0,60]';%[0, 30, 60, 90]'; % Incident wave propagation heading direction (degrees) 
+Tmax        = 14;             % total run time, seconds onshr: 10 mins, offshr: one hour
+seaStates   = [1:2];
+seedSelect  = [1];
+waveDirs    = [0];%[0, 30, 60, 90]; % Incident wave propagation heading direction (degrees) 
 
-disp(['Number of simulatoin:    Nsim = ', num2str(Nsim)]);
+disp(['Number of simulatoin:    Nsim = ', num2str(size(seedSelect, 2))]);
 disp(['Total simulation time:   Tmax = ', num2str(Tmax)]);
 disp(['Wave directions(deg):    ', num2str(waveDirs')]);
 
@@ -74,40 +75,45 @@ changeline(ADipt,10,    ['"',ADwnd,'"']         ,nLines_ADipt);     % Wind file 
 changeline(TSinp,21,    num2str(Tmax, '% 10.1f'),nLines_TSinp);     % Length of analysis time series
 changeline(TSinp,22,    num2str(Tmax, '% 10.1f'),nLines_TSinp);     % Usable length of output time series
 %%
-for iseaState =  %1 : 10 : size(windVel, 1) %
+for iseaState = 1:size(seaStates,2) %  1 : 10 : size(windVel, 1) %
+    windVel = phyRvs(seaStates(1, iseaState),1);
+    waveHs  = phyRvs(seaStates(1, iseaState),2);
+    waveTp  = phyRvs(seaStates(1, iseaState),3);
+
 %     disp(['Wind Velocity    ', num2str(windVel(iseaState)), 'm/s'])
-    for iwaveDir = 1 : size(waveDirs, 1)
-        waveDir = waveDirs(iwaveDir);
+    for iwaveDir = 1 : size(waveDirs, 2)
+        waveDir = waveDirs(1, iwaveDir);
         changeline(HDipt,18,num2str(waveDir       , '% 10.2f'),nLines_HDipt);% 
-        for iSim = 1:10
+        for iseed = 1: size(seedSelect, 2);
+            seed = seedSelect(iseed);
 %             if (iseaState == 17) && (waveDir == 90)
 %                 continue;
 %             end
 %             if (iseaState == 21) && (waveDir == 30)
 %                 continue;
 %             end
-            disp(['>>>>>>>>>>> Number of Simulation : ', num2str(iSim),'<<<<<<<<<<<<<'])
+            disp(['>>>>>>>>>>> Number of Simulation : ', num2str(seed),'<<<<<<<<<<<<<'])
             disp(['Working Directory:       ', WORKDIR(end-7:end)])
             disp(['Mooring line model:      ', MOORING_MODEL]);
-            disp(['Wind Velocity    ', num2str(windVel(iseaState)), 'm/s'])
-            disp(['wave direction:           ', num2str(waveDir),  ' deg'])
-            disp(['waveHs:          ', num2str(waveHs(iseaState)),  'm'])
-            disp(['waveTp:          ', num2str(waveTp(iseaState)),  's'])
+            disp(['Wind Velocity    ', num2str(windVel), 'm/s'])
+            disp(['wave direction:  ', num2str(waveDir), 'deg'])
+            disp(['waveHs:          ', num2str(waveHs),  'm'])
+            disp(['waveTp:          ', num2str(waveTp),  's'])
 %             outfileName = [MODEL_NAME,MOORING_MODEL, num2str(iseaState, '%02.0f'),num2str(waveDir, '%02.0f'), num2str(iSim, '%02.0f'),'.mat'];
-            outfileName = [MODEL_NAME,MOORING_MODEL, num2str(waveDir, '%02.0f'), num2str(iseaState, '%02.0f'), num2str(iSim, '%02.0f'),'.mat'];
+            outfileName = [MODEL_NAME,MOORING_MODEL, num2str(waveDir, '%02.0f'), num2str(seaStates(1, iseaState), '%02.0f'), num2str(seed, '%02.0f'),'.mat'];
     %**************constnat wind profile*******************
     %     changeline(ADipt,10, ['"',Windipt,'"'] ,nLines_ADipt);  % Change to const wind file
     %     changeWindLine (Windipt,4,num2str(windVel(DLC),'% 10.2f'),nLines_Wnd);
     %     changeWindLine (Windipt,5,num2str(windVel(DLC),'% 10.2f'),nLines_Wnd);
     %     changeWindLine (Windipt,6,num2str(windVel(DLC),'% 10.2f'),nLines_Wnd);
     %************** TurbSim input file*******************
-            changeline(TSinp,4, num2str(SEEDS(iSim,1), '% 10.0f'),nLines_TSinp);        % 1st random windseed
-            changeline(TSinp,5, num2str(SEEDS(iSim,2), '% 10.0f'),nLines_TSinp);        % 1st random windseed
-            changeline(HDipt,23,num2str(SEEDS(iSim,3), '% 10.0f'),nLines_HDipt);        % 1st random waveseed
-            changeline(HDipt,24,num2str(SEEDS(iSim,4), '% 10.0f'),nLines_HDipt);        % 2nd random waveseed
-            changeline(TSinp,37,num2str(windVel(iseaState), '% 10.2f'),nLines_TSinp);  % Uref
-            changeline(HDipt,13,num2str(waveHs(iseaState) , '% 10.2f'),nLines_HDipt);  % waveHs
-            changeline(HDipt,14,num2str(waveTp(iseaState) , '% 10.2f'),nLines_HDipt);  % waveTp
+            changeline(TSinp,4, num2str(SEEDS(seed,1), '% 10.0f'),nLines_TSinp);        % 1st random windseed
+            changeline(TSinp,5, num2str(SEEDS(seed,2), '% 10.0f'),nLines_TSinp);        % 1st random windseed
+            changeline(HDipt,23,num2str(SEEDS(seed,3), '% 10.0f'),nLines_HDipt);        % 1st random waveseed
+            changeline(HDipt,24,num2str(SEEDS(seed,4), '% 10.0f'),nLines_HDipt);        % 2nd random waveseed
+            changeline(TSinp,37,num2str(windVel, '% 10.2f'),nLines_TSinp);  % Uref
+            changeline(HDipt,13,num2str(waveHs,  '% 10.2f'),nLines_HDipt);  % waveHs
+            changeline(HDipt,14,num2str(waveTp,  '% 10.2f'),nLines_HDipt);  % waveTp
             % Run TurbSim
             command = ['SNL13MW\Wind\TurbSim.exe SNL13MW\Wind\',MODEL_NAME,'_Wind.inp'];
             system(command);
@@ -120,8 +126,13 @@ for iseaState =  %1 : 10 : size(windVel, 1) %
             data = hdrload([MODEL_NAME,'.out']);
             [outlist,units]=getoutlist([MODEL_NAME, '.sum']); 
             save(OutputDir,'data','outlist','units');
-            
+            command = ['C:\software\gdrive.exe upload -p 0BymogEaRlu2lSGtTOU1kZzZKRzQ',' ', OutputDir];
+            system(command)
+       
         end
     end
 end
 delete([MODEL_NAME,'.out']);
+% setpref('Internet','E_mail','jinsongliu@utexas.edu');
+% setpref('Internet','SMTP_Server','smtp.gmail.com');
+% sendmail('jinsongliu@utexas.edu', [num2str(seaStates(1,1)), ' - ', num2str(seaStates(1,end)), ' Done!'], 'uploaded')
